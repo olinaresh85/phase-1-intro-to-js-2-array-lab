bae59801cca25fbfdac560d510d91aa7a6f13861
Naresh Oli <olinaresh85@gmail.com>
2023-03-25 14:27:05 -0700

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name="Ralph") {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name="Bob") {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name = "Broom") {
+    return [...cats, name];
+}
+
+function prependCat(name="Arnold") {
+    return [name,...cats];
+}
+
+function removeLastCat() {
+    let newArray = cats.slice(0,-1);
+    return newArray;
+}
+
+function removeFirstCat()  {
+    return cats.slice(1);
+}
